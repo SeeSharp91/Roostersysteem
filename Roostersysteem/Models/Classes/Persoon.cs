fbf89f002e927218d88f89b08ab6b9648f720f09
Max Thomas <1328565thomas@zuyd.nl>
2018-11-02 12:03:45 +0100

    update login usings

--- Roostersysteem/Models/Classes/Persoon.cs
+++ Roostersysteem/Models/Classes/Persoon.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Net.Mail;
+using System.Text;
 
 namespace Roostersysteem.Models
 {
